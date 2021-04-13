750690a2a4935f917b6ad3013aff89859ca0c1ae
Joshua Hegedus <josh.hegedus@outlook.com>
2021-04-13 21:02:11 +0200

    Drag, and application exit with the 'X' button is done.
    *Don't forget to replace the image to your absolute path(right click in the image->copy path->absolute path) and put it in your MainWindow.xaml.cs* Also for the medals in the HomeView.cs

--- NewCompetitionAnalizer/MainWindow.xaml.cs
+++ NewCompetitionAnalizer/MainWindow.xaml.cs
@@ -1,21 +1,30 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace NewCompetitionAnalizer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            
+            this.MouseDown += Window_MouseDown;
+            string path = Path.GetFullPath("Images/image.jpg");
+            CloseImage.Source = new BitmapImage(new Uri(@"D:\学校\Jetbrains\C#\Tesztverseny\NewCompetitionAnalizer\Images\close_button.png"));
+
         }
 
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
         private void ExitProgram(object sender, RoutedEventArgs e)
         {
-            // throw new System.NotImplementedException();
+            this.Close();
         }
     }
 }
